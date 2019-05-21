0563d777dbf1aea877a3c482618724b923ae9870
Kelly Zick <kllyzck@gmail.com>
2019-05-21 17:34:28 -0600

    GET method successfully returns mockList
    Co-authored-by: Mark R. Collins <markcollins.md@gmail.com>
    Co-authored-by: Theo Bean <beantheo@gmail.com>

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
-
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
 app.locals.title = 'trapper-keeper';
+const mockList = {title: 'example', id: 1, listItems: [{id: 111, text: 'example list item'}]}
 
-app.get('/', (request, response) => {
-  response.send('Hello team!')
+app.locals.keeper = [mockList];
+
+app.use(express.json());
+
+app.get('/api/v1/keeper', (request, response) => {
+  response.status(200).json(app.locals.keeper)
 })
 
 app.listen(app.get('port'), () => {
