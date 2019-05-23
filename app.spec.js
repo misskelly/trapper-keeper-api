d76c054e2f4defc177e962a0737e101563da5a5b
kobesparrow <markcollins.md@gmail.com>
2019-05-23 11:29:40 -0600

    Test get and get by id

--- app.spec.js
+++ app.spec.js
@@ -1,23 +1,64 @@
+import request from 'supertest';
+import '@babel/polyfill';
+import app from './app.js';
+import { exportAllDeclaration } from '@babel/types';
+
 describe('api', () => {
   describe('GET /api/v1/notes', () => {
-    it('should have a status of 200', () => {
+    
+    it('should have a status of 200', async () => {
+      const response = await request(app).get('/api/v1/notes');
 
+      expect(response.statusCode).toBe(200);
     });
-    it('should return an array of notes', () => {
 
+    it('should return an array of notes', async () => {
+      let mockNotes = [
+        { title: 'example', id: 1, listItems: [{ id: 111, text: 'example list item' }] },
+        { title: 'note', id: 2, listItems: [{ id: 222, text: 'another example' }] }
+      ]
+      app.locals.notes = mockNotes
+
+      const response = await request(app).get('/api/v1/notes')
+
+      expect(response.body).toEqual(mockNotes)
     });
   });
+
   describe('GET /api/v1/notes/:id', () => {
-    it('should have a status of 200', () => {
+    let mockNotes
+    let errorCode
+    let goodNote
 
-    });
-    it('should return a specific note based on ID', () => {
+    beforeEach(() => {
+      mockNotes = [
+        { title: 'example', id: 1, listItems: [{ id: 111, text: 'example list item' }] },
+        { title: 'note', id: 2, listItems: [{ id: 222, text: 'another example' }] }
+      ]
+      app.locals.notes = mockNotes
+    })
 
-    });
-    it('should have a status of 404 if page not found', () => {
+    it('should have a status of 200', async () => {
+      const response = await request(app).get('/api/v1/notes/1')
 
+      expect(response.statusCode).toBe(200)
     });
-    it('should return error message', () => {
+
+    it('should return a specific note based on ID', async () => {
+      goodNote = { title: 'example', id: 1, listItems: [{ id: 111, text: 'example list item' }] }
+      
+      const response = await request(app).get('/api/v1/notes/1')
+
+      expect(response.body).toEqual(goodNote)
+    });
+
+    it('should have a status of 404 if page not found', async () => {
+      const response = await request(app).get('/api/v1/notes/69');
+
+      expect(response.statusCode).toBe(404);
+    });
+    
+    it.skip('should return error message', () => {
 
     });
   });
